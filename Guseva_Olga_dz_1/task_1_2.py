e4595b4c78a984e0bb5a7123ffe6b2ed801596f9
gusevaolga <olyaguseva2008@rambler.ru>
2021-03-16 19:57:25 +0300

    attempt for task_1_2

--- Guseva_Olga_dz_1/task_1_2.py
+++ Guseva_Olga_dz_1/task_1_2.py
@@ -1,21 +1,22 @@
-spisok = ['1111']
+list_numb = []
+spisok_1 = []
+for numb in range (1, 1001, 2):
+    numb_cube = (numb ** 3)
+    list_numb.append(numb_cube)
 
-for a in range (1, 1001, 2):
-    b = (a**3)
+summ_numb = 0
+result_numb = 0
 
-    c = 0
-    while b > 0:
-        c = c + (b % 10)
-        b = b // 10
-
-    res = (c % 7)
-    if res == 0:
-        res1 = str(a**3)
-        res2 = res1 +
-
-
-
-
+for i in list_numb:
+    test_elem = i
+    while test_elem > 0:
+        summ_numb = summ_numb + (test_elem % 10)
+        test_elem = test_elem // 10
+    spisok_1.append(summ_numb)
+    print(spisok_1)
+    if spisok_1 % 7 == 0:
+        result_numb += spisok_1
+        print(result_numb)
 
 
 
