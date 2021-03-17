f9d3c5d729a5d5c1fa5325cb49068d64eb75fd19
gusevaolga <olyaguseva2008@rambler.ru>
2021-03-18 02:22:59 +0300

    finished task_1_2

--- Guseva_Olga_dz_1/task_1_2.py
+++ Guseva_Olga_dz_1/task_1_2.py
@@ -1,23 +1,32 @@
-list_numb = []
-spisok_1 = []
-for numb in range (1, 1001, 2):
-    numb_cube = (numb ** 3)
-    list_numb.append(numb_cube)
 
-summ_numb = 0
-result_numb = 0
+numb_list = []
 
-for i in list_numb:
-    test_elem = i
-    while test_elem > 0:
-        summ_numb = summ_numb + (test_elem % 10)
-        test_elem = test_elem // 10
-    spisok_1.append(summ_numb)
-    print(spisok_1)
-    if spisok_1 % 7 == 0:
-        result_numb += spisok_1
-        print(result_numb)
+for item in range (1, 1001, 2):
+    numb_list.append(item ** 3)
 
+result_sum = 0
+for number in numb_list:
+    number_summ = 0
+    item = number
+    while number != 0:
+        number_summ += (number % 10)
+        number = number // 10
+    if number_summ % 7 == 0:
+        result_sum += item
+
+for dev in numb_list:
+    dev_summ = dev + 17
+    resul_dev = dev_summ
+
+    dev_i = 0
+    dev_finish = 0
+
+    while resul_dev != 0:
+        dev_i += (resul_dev % 10)
+        resul_dev = resul_dev // 10
+    if dev_i % 7 == 0:
+        dev_finish += dev_summ
+        print(dev_finish)
 
 
 
