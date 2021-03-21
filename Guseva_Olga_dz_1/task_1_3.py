30f8e9fe36a9d6635adc614732eecbe9fe1481d3
gusevaolga <olyaguseva2008@rambler.ru>
2021-03-21 18:31:03 +0300

    fixed task_1_3

--- Guseva_Olga_dz_1/task_1_3.py
+++ Guseva_Olga_dz_1/task_1_3.py
@@ -1,11 +1,9 @@
 org_word = str("Процент")
 key_input = int(input("Введите число:"))
 
-if key_input == 0:
+if key_input > 4 and key_input <= 20 or key_input == 0:
     print(str(key_input) + " " + (org_word+str("ов.")))
 elif key_input > 0 and key_input < 2:
     print(str(key_input) + " " + (org_word) + str("."))
 elif key_input > 1 and key_input < 4:
     print(str(key_input) + " " + (org_word) + str("а."))
-elif key_input > 4 and key_input <= 20:
-    print(str(key_input) + " " + (org_word) + str("ов."))
