92d897664da7db1894998e38c12fb24da6ba30f5
gusevaolga <olyaguseva2008@rambler.ru>
2021-04-01 17:19:50 +0300

    task_4_2 completed

---
+++ Guseva_Olga_dz_4/task_4_2.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def currency_rates (code):
+    upper_code = code.upper()
+    response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+    result_dict = json.loads(response.content)
+    valute = result_dict['Valute'].get(upper_code)
+    if valute:
+        value = valute['Value']
+        return value
+
+print(currency_rates('usd'))
+print(currency_rates('eur'))
+
+
+
